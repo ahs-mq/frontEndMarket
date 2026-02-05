c43e104f63b3abe477411adc1bf8c51cb4247c30
ahs-mq <141071955+ahs-mq@users.noreply.github.com>
2026-02-05 15:18:47 +0400

    removed unused echo.js file as window.echo isnt used

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,8 @@
 
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react';
-import './echo.jsx'
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
 import App from './App.jsx'
 
-window.Pusher = Pusher;
-
-window.Echo = new Echo({
-  broadcaster: 'reverb',
-  key: import.meta.env.VITE_REVERB_APP_KEY,
-  wsHost: import.meta.env.VITE_REVERB_HOST || '127.0.0.1',
-  wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-  wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-  forceTLS: false,
-  enabledTransports: ['ws', 'wss'],
-});
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
